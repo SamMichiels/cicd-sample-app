e08271d51b407c8f053abd69c03f378fadeb7936
SamMichiels <76103176+SamMichiels@users.noreply.github.com>
2023-10-24 15:19:32 +0200

    Update sample-app.sh

--- sample-app.sh
+++ sample-app.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
+if [ -d "/tempdir" ]
 mkdir tempdir
 mkdir tempdir/templates
 mkdir tempdir/static
@@ -19,6 +20,7 @@ EXPOSE 5050
 CMD python /home/myapp/sample_app.py
 _EOF_
 
+fi
 cd tempdir || exit
 docker build -t sampleapp .
 docker run -t -d -p 5050:5050 --name samplerunning sampleapp
