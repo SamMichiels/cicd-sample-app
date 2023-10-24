a5e3e79bcd0f5ff2e58638819b5290986cf97395
SamMichiels <76103176+SamMichiels@users.noreply.github.com>
2023-10-24 15:24:27 +0200

    Update sample-app.sh

--- sample-app.sh
+++ sample-app.sh
@@ -2,24 +2,24 @@
 set -euo pipefail
 
 if [ ! -d "/tempdir" ]
-  then
-    mkdir tempdir
-    mkdir tempdir/templates
-    mkdir tempdir/static
+then
+  mkdir tempdir
+  mkdir tempdir/templates
+  mkdir tempdir/static
     
-    cp sample_app.py tempdir/.
-    cp -r templates/* tempdir/templates/.
-    cp -r static/* tempdir/static/.
+  cp sample_app.py tempdir/.
+  cp -r templates/* tempdir/templates/.
+  cp -r static/* tempdir/static/.
     
-    cat > tempdir/Dockerfile << _EOF_
-    FROM python
-    RUN pip install flask
-    COPY  ./static /home/myapp/static/
-    COPY  ./templates /home/myapp/templates/
-    COPY  sample_app.py /home/myapp/
-    EXPOSE 5050
-    CMD python /home/myapp/sample_app.py
-    _EOF_
+  cat > tempdir/Dockerfile << _EOF_
+  FROM python
+  RUN pip install flask
+  COPY  ./static /home/myapp/static/
+  COPY  ./templates /home/myapp/templates/
+  COPY  sample_app.py /home/myapp/
+  EXPOSE 5050
+  CMD python /home/myapp/sample_app.py
+_EOF_
 fi
 cd tempdir || exit
 docker build -t sampleapp .
